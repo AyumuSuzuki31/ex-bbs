501fcc06fd74b522c88c0146d63e99506dabfb7b
AyumuSuzuki <alxd3380@gmail.com>
2025-08-22 13:10:13 +0900

    リポジトリクラスにfindbyarticleidメソッドを定義

--- src/main/java/com/example/ex_bbs/repository/CommentRepository.java
+++ src/main/java/com/example/ex_bbs/repository/CommentRepository.java
@@ -1,5 +1,9 @@
 package com.example.ex_bbs.repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -30,5 +34,21 @@ public class CommentRepository {
     comment.setArticleId(rs.getInt("article_id"));
     return comment;
   };
-   
+
+  /**
+   * 指定された記事IDのコメントを取得します.
+   * id昇順で並べ替えます。
+   * 
+   * @param articleId 記事ID
+   * @return コメント一覧(id昇順)
+   */
+
+   public List<Comment> findByArticleId(int articleId) {
+    String sql = "SELECT id, name, content, article_id FROM comments WHERE article_id = :articleId ORDER BY id ASC";
+    
+    Map<String, Object> param = new HashMap<>(); 
+    param.put("articleId", articleId);
+
+    return template.query(sql, param, COMMENT_ROW_MAPPER);
+   }
 }
