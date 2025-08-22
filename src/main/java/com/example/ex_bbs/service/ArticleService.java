0032b57e1b72c95c919dad1969ec98bb6e77e769
AyumuSuzuki <alxd3380@gmail.com>
2025-08-22 13:50:27 +0900

    transactionalアノテーションの追記、リクエストマッピングを/boardに変更

--- src/main/java/com/example/ex_bbs/service/ArticleService.java
+++ src/main/java/com/example/ex_bbs/service/ArticleService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.ex_bbs.domain.Article;
 import com.example.ex_bbs.repository.ArticleRepository;
@@ -14,6 +15,7 @@ import com.example.ex_bbs.repository.ArticleRepository;
  * @author suzukiayumu
  */
 @Service
+@Transactional
 public class ArticleService {
     
     @Autowired
