6405862b4edce8725c6e06b42d2e25290ba07c89
AyumuSuzuki <alxd3380@gmail.com>
2025-08-22 13:24:35 +0900

    ArticleServiceクラスにfindAllWithCommentメソッドを定義

--- src/main/java/com/example/ex_bbs/service/ArticleService.java
+++ src/main/java/com/example/ex_bbs/service/ArticleService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.ex_bbs.domain.Article;
+import com.example.ex_bbs.domain.Comment;
 import com.example.ex_bbs.repository.ArticleRepository;
+import com.example.ex_bbs.repository.CommentRepository;
 
 /**
  * 記事に関するビジネスロジックを提供するサービスクラスです.
@@ -18,6 +20,9 @@ public class ArticleService {
     
     @Autowired
     private ArticleRepository articleRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
     
     /**
      * 全ての記事を取得します.
@@ -36,4 +41,23 @@ public class ArticleService {
     public void insert(Article article) {
       articleRepository.insert(article);
     }
+
+    /**
+     * 全ての記事とそれに関するコメントを取得します.
+     * 
+     * @return コメント付き記事一覧
+     */
+
+     public List<Article> findAllWithComment() {
+      List<Article> articles = articleRepository.findAll();
+
+      for (Article article : articles) {
+        List<Comment> comments = commentRepository.findByArticleId(article.getId());
+        article.setCommentList(comments);
+      }
+
+      return articles;
+     }
+
+
 }
