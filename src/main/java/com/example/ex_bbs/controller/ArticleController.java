71c099b4554c7a241ce9dc8521b20154a518f24e
AyumuSuzuki <alxd3380@gmail.com>
2025-08-22 13:40:31 +0900

    コントローラクラスのrequestmappingのパスをboardに変更

--- src/main/java/com/example/ex_bbs/controller/ArticleController.java
+++ src/main/java/com/example/ex_bbs/controller/ArticleController.java
@@ -8,8 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.ex_bbs.domain.Article;
-import com.example.ex_bbs.service.ArticleService;
 import com.example.ex_bbs.form.ArticleForm;
+import com.example.ex_bbs.service.ArticleService;
 
 /**
  * 記事一覧画面を表示するクラスです.
@@ -18,7 +18,7 @@ import com.example.ex_bbs.form.ArticleForm;
  */
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/board")
 public class ArticleController {
 
   @Autowired
